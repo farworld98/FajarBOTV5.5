051758aca45181a97d7125b39277f8d851bd0e95
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-01-28 08:22:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ console.log('Starting...')
 let { spawn } = require('child_process')
 let path = require('path')
 const CFonts  = require('cfonts')
-CFonts.say('TERMUX WHATSAPP BOT', {
+CFonts.say('Fajar & Debby', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('\'wabot-aq\' By @Nurutomo', {
+CFonts.say('\'𝙼𝚎𝚐𝚞𝚖𝚒𝚗 𝙱𝙾𝚃\' By @mhmdfjralfarizi_', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
